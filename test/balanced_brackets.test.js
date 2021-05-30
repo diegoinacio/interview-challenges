2d993f0680bb627d3f74ef669cf0dae238ebfcb7
Diego Inácio <diegodci@gmail.com>
2021-05-30 15:38:15 -0300

    Upload test

---
+++ test/balanced_brackets.test.js
@@ -0,0 +1,31 @@
+const assert = require("chai").assert;
+const isBalanced = require("../balanced_brackets");
+
+describe("Sample Tests", function () {
+  describe("Case 0", function () {
+    assert.equal(isBalanced("{[()]}"), "YES");
+    assert.equal(isBalanced("{[(])}"), "NO");
+    assert.equal(isBalanced("{{[[(())]]}}"), "YES");
+  });
+
+  describe("Case 1", function () {
+    assert.equal(isBalanced("{{([])}}"), "YES");
+    assert.equal(isBalanced("{{)[](}}"), "NO");
+  });
+
+  describe("Case 2", function () {
+    assert.equal(isBalanced("{(([])[])[]}"), "YES");
+    assert.equal(isBalanced("{(([])[])[]]}"), "NO");
+    assert.equal(isBalanced("{(([])[])[]}[]"), "YES");
+  });
+});
+
+describe("Custom Tests", function () {
+  describe("Case 0", function () {
+    assert.equal(isBalanced(""), "YES");
+    assert.equal(isBalanced("13"), "YES");
+    assert.equal(isBalanced("([{7}])"), "YES");
+    assert.equal(isBalanced("{[()*15{}]}"), "YES");
+    assert.equal(isBalanced("{[{[{()}]}])"), "NO");
+  });
+});
