5dd774a248c052fb31161ee50ac429b8a1c11401
Diego Inácio <diegodci@gmail.com>
2021-05-30 14:44:08 -0300

    Update challange

--- balanced_brackets.py
+++ balanced_brackets.py
@@ -7,7 +7,7 @@
 
 # ? Function Description
 
-# It must return a string: YES if the sequence is balanced 
+# It must return a string: YES if the sequence is balanced
 # or NO if it is not.
 
 # "isBalanced" has the following parameter(s):
@@ -15,12 +15,12 @@
 
 
 def isBalanced(input_string: str) -> bool:
-    # Define valid bracket pairs
+    # * Define valid bracket pairs
     brackets = [r'()', r'[]', r'{}']
-    # Remove extra information from input string
-    input_string = ''.join(e for e in input_string if e in ''.join(brackets))
-    # Loop while exist any pair in the input string
+    # * Remove extra information from input string
+    input_string = "".join(e for e in input_string if e in "".join(brackets))
+    # * Loop while exist any pair in the input string
     while any(b in input_string for b in brackets):
-        for bracket in brackets:
-            input_string = input_string.replace(bracket, '')
-    return 'YES' if not input_string else 'NO'
+        for b in brackets:
+            input_string = input_string.replace(b, '')
+    return "NO" if input_string else "YES"
