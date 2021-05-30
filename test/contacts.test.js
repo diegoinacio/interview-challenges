6ab47b3a4ef92800b61c5fd90418ed6bfeab0ee8
Diego Inácio <diegodci@gmail.com>
2021-05-30 17:12:42 -0300

    Upload test

---
+++ test/contacts.test.js
@@ -0,0 +1,64 @@
+const assert = require("chai").assert;
+const contacts = require("../contacts");
+
+describe("Sample Tests", function () {
+  describe("Case 0", function () {
+    assert.deepEqual(
+      contacts([
+        ["add", "hack"],
+        ["add", "hackerrank"],
+        ["find", "hac"],
+        ["find", "hak"],
+      ]),
+      [2, 0]
+    );
+  });
+
+  describe("Case 1", function () {
+    assert.deepEqual(
+      contacts([
+        ["add", "ed"],
+        ["add", "eddie"],
+        ["add", "edward"],
+        ["find", "ed"],
+        ["add", "edwina"],
+        ["find", "edw"],
+        ["find", "a"],
+      ]),
+      [3, 2, 0]
+    );
+  });
+});
+
+describe("Custom Tests", function () {
+  describe("Case 0", function () {
+    assert.deepEqual(
+      contacts([
+        ["add", "diego"],
+        ["add", "diogo"],
+        ["add", "diogenes"],
+        ["find", "die"],
+        ["find", "dio"],
+      ]),
+      [1, 2]
+    );
+  });
+
+  describe("Case 1", function () {
+    assert.deepEqual(
+      contacts([
+        ["add", "diego"],
+        ["add", "diogo"],
+        ["find", "die"],
+        ["add", "diogenes"],
+        ["find", "dio"],
+        ["add", "diegones"],
+        ["add", "dialogo"],
+        ["find", "die"],
+        ["find", "dio"],
+        ["find", "dia"],
+      ]),
+      [1, 2, 2, 2, 1]
+    );
+  });
+});
---
+++ test/running_median.test.js
@@ -0,0 +1,32 @@
+const assert = require("chai").assert;
+const runningMedian = require("../running_median");
+
+describe("Sample Tests", function () {
+  describe("Case 0", function () {
+    assert.deepEqual(runningMedian([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]), [
+      "1.0",
+      "1.5",
+      "2.0",
+      "2.5",
+      "3.0",
+      "3.5",
+      "4.0",
+      "4.5",
+      "5.0",
+      "5.5",
+    ]);
+  });
+});
+
+describe("Custom Tests", function () {
+  describe("Case 0", function () {
+    assert.deepEqual(runningMedian([]), []);
+    assert.deepEqual(runningMedian([16, 8, 2, 4, 32]), [
+      "16.0",
+      "12.0",
+      "8.0",
+      "6.0",
+      "8.0",
+    ]);
+  });
+});
