95dd10a712a82c7fcb49af2bd84fb1400f2046f4
Diego Inácio <diegodci@gmail.com>
2021-06-01 12:53:01 -0300

    Upload src

---
+++ src/tree_algorithms.js
@@ -0,0 +1,119 @@
+class Node {
+  constructor(data) {
+    this._data = data;
+    this._left = null;
+    this._right = null;
+  }
+
+  get data() {
+    return this._data;
+  }
+
+  set data(value) {
+    this._data = value;
+  }
+
+  get left() {
+    return this._left;
+  }
+
+  set left(value) {
+    this._left = new Node(value);
+  }
+
+  get right() {
+    return this._right;
+  }
+
+  set right(value) {
+    this._right = new Node(value);
+  }
+}
+
+class BinaryTree {
+  constructor(value = null) {
+    this._root = value && new Node(value);
+  }
+
+  get root() {
+    return this._root;
+  }
+
+  set root(value) {
+    this._root = new Node(value);
+  }
+}
+
+function get_preOrder(root, order = []) {
+  if (root) {
+    // * First get the data of node
+    order.push(root.data);
+    // * Then recur on left child
+    get_preOrder(root.left, order);
+    // * Finally recur on right child
+    get_preOrder(root.right, order);
+  }
+  // * Return a list of order
+  return order;
+}
+
+function get_postOrder(root, order = []) {
+  if (root) {
+    // * First recur on left child
+    get_postOrder(root.left, order);
+    // * Then recur on right child
+    get_postOrder(root.right, order);
+    // * Finally get the data of node
+    order.push(root.data);
+  }
+  // * Return a list of order
+  return order;
+}
+
+function get_inOrder(root, order = []) {
+  if (root) {
+    // * First recur on left child
+    get_inOrder(root.left, order);
+    // * Then get the data of node
+    order.push(root.data);
+    // * Finally recur on right child
+    get_inOrder(root.right, order);
+  }
+  // * Return a list of order
+  return order;
+}
+
+function get_levelOrder(root, order = []) {
+  if (root) {
+    // * Init stacking the root note
+    const stack = [root];
+    // * While the stack is not empty
+    while (stack.length) {
+      // * Get the first element
+      let node = stack.shift();
+      if (node) {
+        // * Store the level order
+        order.push(node.data);
+        // * Stack the left and right nodes
+        stack.push(node.left, node.right);
+      }
+    }
+  }
+  // * Return a list of order
+  return order;
+}
+
+function get_height(root) {
+  return root
+    ? Math.max(get_height(root.left), get_height(root.right)) + 1
+    : -1;
+}
+
+module.exports = {
+  BinaryTree,
+  get_preOrder,
+  get_postOrder,
+  get_inOrder,
+  get_levelOrder,
+  get_height,
+};
